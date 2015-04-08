b3fbd50a8a8bf33ff36fd84f174d98434db5ae56
Erik Svedäng <erik.svedang@gmail.com>
2015-04-08 16:42:50 +0200

    Initial Commit

---
+++ GlfwTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GlfwTest
+//
+//  Created by Erik Svedäng on 08/04/15.
+//  Copyright (c) 2015 Erik Svedäng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
