797ca238eed73746205c189e4a66ea038dce983d
Erik Svedäng <erik.svedang@gmail.com>
2015-05-01 16:53:22 +0200

    textures!

--- GlfwTest/main.cpp
+++ GlfwTest/main.cpp
@@ -7,7 +7,25 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "ResourcePath.h"
+#include "SOIL.h"
+
 using namespace std;
+
+GLuint LoadTexture(string name) {
+    int width, height;
+    string path = getResourcePath(name, "jpg");
+    unsigned char* image = SOIL_load_image(path.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
+    if(!image) cout << "Failed to load image." << endl;
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    return texture;
+}
 
 int main(void)
 {
@@ -23,7 +41,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Wie geht's?", NULL, NULL);
 
     if (!window) {
         glfwTerminate();
@@ -39,7 +57,7 @@ int main(void)
         printf("Failed to initialize GLEW\n");
         return -1;
     }
-    
+
     GLint compile_ok = GL_FALSE;
     
     /* Vertex shader */
@@ -48,11 +66,15 @@ int main(void)
     const char *vs_source =
     "#version 330 core                                             \n"
     "layout (location = 0) in vec3 position;                         "
+    "layout (location = 1) in vec2 texCoord;                         "
+    "uniform float i;"
     "out float instid;                                               "
+    "out vec2 TexCoord;                                              "
     "void main(void) {                                               "
-    "  float xmod = -0.9 + gl_InstanceID * 0.05f;                    "
+    "  float xmod = -0.9 + i * 0.05f;                    "
     "  instid = gl_InstanceID;                                       "
     "  gl_Position = vec4(position.x + xmod, position.y, position.z, 1.0);  "
+    "  TexCoord = texCoord;                                          "
     "}                                                               ";
     
     glShaderSource(vs, 1, &vs_source, NULL);
@@ -72,9 +94,12 @@ int main(void)
     const char *fs_source =
     "#version 330 core                                      \n"
     "in float instid;                                         "
+    "in vec2 TexCoord;                                        "
+    "uniform sampler2D ourTexture;                            "
     "out vec4 color;                                          "
     "void main(void) {                                        "
-    "  color = vec4(1.0f, 0.5f, instid * 0.1f, 1.0f);                  "
+    " color = texture(ourTexture, TexCoord) * vec4(0.9, 0.9, 1.0, 1.0f);                "
+    //"  color = vec4(1.0f, 0.5f, instid * 0.1f, 1.0f);                  "
     "}                                                        ";
     
     glShaderSource(fs, 1, &fs_source, NULL);
@@ -103,9 +128,13 @@ int main(void)
     }
     
     static const GLfloat triangle_vertices[] = {
-        0.8f, -0.8f, 0.0f,
-        0.9f, -0.6f, 0.0f,
-        0.0f,  0.8f, 0.0f,
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f,   // Top Right
+        0.5f, -0.5f, 0.0f,   1.0f, 1.0f,   // Bottom Right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f,   // Bottom Left
+        
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f,   // Bottom Left
+        0.5f, 0.5f, 0.0f,   1.0f, 0.0f,   // Bottom Right
+        -0.5f,  0.5f, 0.0f,  0.0f, 0.0f    // Top Left
     };
 
     GLuint vao;
@@ -123,29 +152,58 @@ int main(void)
 //        fprintf(stderr, "Could not bind attribute %s\n", attribute_name);
 //        return 0;
 //    }
+    
+    
+    // TEXTURE!
+    
+    int texture = LoadTexture("jelly");
+    int texture2 = LoadTexture("bleh");
+    
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindTexture(GL_TEXTURE_2D, texture);
 
     glClearColor(0.98, 0.98, 0.98, 1.0);
+    
+    GLuint uniform_i = glGetUniformLocation(program, "i");
 
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
         
         glUseProgram(program);
-        glEnableVertexAttribArray(0);
         
         /* Describe our vertices array to OpenGL (it can't guess its format automatically) */
         glVertexAttribPointer(0, // attribute
+                              3,                 // number of elements per vertex, here (x,y)
+                              GL_FLOAT,          // the type of each element
+                              GL_FALSE,          // take our values as-is
+                              5 * sizeof(float),                 // no extra data between each position
+                              0
+                              );
+        glEnableVertexAttribArray(0);
+        
+        glVertexAttribPointer(1, // attribute
                               2,                 // number of elements per vertex, here (x,y)
                               GL_FLOAT,          // the type of each element
                               GL_FALSE,          // take our values as-is
-                              0,                 // no extra data between each position
-                              0
+                              5 * sizeof(float),                 // no extra data between each position
+                              (const void*)(3 * sizeof(float))
                               );
+        glEnableVertexAttribArray(1);
         
-        glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 20);
+        glUniform1f(uniform_i, 0.0f);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glDrawArrays(GL_TRIANGLES, 0, 20);
+
+        glUniform1f(uniform_i, 20.0f);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glDrawArrays(GL_TRIANGLES, 0, 20);
+        
         glDisableVertexAttribArray(0);
-        
+        glDisableVertexAttribArray(1);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
